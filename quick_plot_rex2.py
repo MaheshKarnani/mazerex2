356f2b1d6295f77dbb290f0b857abffbefe68cdb
MaheshKarnani <36512140+MaheshKarnani@users.noreply.github.com>
2025-02-07 15:11:55 +0000

    Update quick_plot_rex2.py
    12h precision and phase markers added

--- quick_plot_rex2.py
+++ quick_plot_rex2.py
@@ -3,18 +3,19 @@ import scipy.signal as sig
 import numpy as np
 import pandas as pd
 from IPython.display import display
-from datetime import datetime, date, timedelta
+from datetime import datetime, date, timedelta, time
 import matplotlib.dates as mdates
 import statistics
 plt.close('all')
 
+start_date=date(2025,1,29) 
+marker_times=[datetime(2025,2,1,12,0,0),datetime(2025,2,4,12,0,0) ,datetime(2025,2,7,12,0,0)] #add important dates here to add vertical lines on last plot
 last_date=date.today() #OR TYPE DESIRED DATE ON NEXT LINE AND UNCOMMENT IT
 # last_date=date(2025,2,3)
 datetag=str(last_date)
 known_tags=[196447011,19645674,19645246186,19644148217,19647144222]
 filtermin=13 #lower limit in g
 filtermax=23 #upper limit in g 
-start_date=date(2025,1,29) 
 d=last_date-start_date
 days_to_plot=d.days
          
@@ -94,23 +95,35 @@ ax3.set_xticklabels(x,rotation=30,ha='right')
 ax3.set_ylim([12, 20])
 
 # gather and plot daily averages of known animals
-matrix=[averages]
-x=[date.today()]
-for j in range(days_to_plot-1):
-    day=date.today()-timedelta(days = j+1)
-    x.append(day)
-    data = pd.read_csv(loadpath + str(day) + "_events.csv")
+matrix=[]
+x=[]
+for j in range(days_to_plot):
+    day=date.today()-timedelta(days = j) 
+    data = pd.read_csv(loadpath + str(day) + "_events.csv") #filter time to 12h bin
     # plot filtered weights of known animals and gather averages
-    averages=[]
+    bin1_offset=time(18,0)
+    bin1_centre=datetime.combine(day,bin1_offset)
+    bin1_start_offset=time(12,0)
+    bin1_start=datetime.combine(day,bin1_start_offset)
+    x.append(bin1_centre)
+    bin2_offset=time(6,0)
+    bin2_centre=datetime.combine(day,bin2_offset)
+    x.append(bin2_centre)
+    averages1=[]
+    averages2=[]
     for i in range(len(known_tags)):
         filtered_an = data.loc[data['Animal'] == known_tags[i]] 
         filtered_min = filtered_an.loc[filtered_an['Weight'] > filtermin]
         filtered_minmax =  filtered_min.loc[filtered_min['Weight'] < filtermax]
-        averages.append(statistics.mean(filtered_minmax['Weight']))
-    matrix.append(averages)
+        filtered_time = filtered_minmax.loc[pd.to_datetime(filtered_minmax['Start_Time'], format='%Y-%m-%d %H:%M:%S.%f') > bin1_start]
+        averages1.append(statistics.mean(filtered_time['Weight']))
+        filtered_time = filtered_minmax.loc[pd.to_datetime(filtered_minmax['Start_Time'], format='%Y-%m-%d %H:%M:%S.%f') < bin1_start]
+        averages2.append(statistics.mean(filtered_time['Weight']))
+    matrix.append(averages1)
+    matrix.append(averages2)
 print(x)
 matrix1=list(map(list, zip(*matrix)))
-print(matrix1)
+# print(matrix1)
 ax4=fig1.add_subplot(224)
 ax4.set_title(f"daily mean of {len(known_tags)} known tags")
 print(len(known_tags))
@@ -118,6 +131,8 @@ for i in range(len(known_tags)):
     print(i)
     print(matrix1[i])
     ax4.plot(x , matrix1[i], alpha=.5)
+for i in range(len(marker_times)):
+    ax4.plot([marker_times[i],marker_times[i]],[filtermin, filtermax])
 ax4.set_ylabel("weight, g")
 ax4.set_xticks(x)
 ax4.set_xticklabels(x,rotation=30,ha='right')
